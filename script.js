3754f92d2d0e9eb19e66067379644e5b97113822
tr3ndy661 <trendypotato661@gmail.com>
2025-07-26 16:54:23 +0300

    done with making the basic structure

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello from JS!");
+const modal = document.getElementById('moodModal');
+const closeBtn = document.querySelector('.close-button');
+const dayTiles = document.querySelectorAll('.day-tile');
+
+dayTiles.forEach(tile => {
+  tile.addEventListener('click', () => {
+    modal.style.display = 'flex';
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
